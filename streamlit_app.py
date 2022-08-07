cdcf65c998818ddfc580e74430fd59dd6381c53a
Venkatesh Narayana Raja <110780431+venkysnow@users.noreply.github.com>
2022-08-07 23:57:39 +0530

    Update streamlit_app.py
    Added gif and avocado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
-
+  
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
