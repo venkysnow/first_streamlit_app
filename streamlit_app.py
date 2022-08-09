a92f6fb2022828d9ac38a08d4e5eb39e4fef85ea
Venkatesh Narayana Raja <110780431+venkysnow@users.noreply.github.com>
2022-08-09 20:55:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 #  normalise the semi structured data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
