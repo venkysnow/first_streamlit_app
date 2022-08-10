129ae293fd0298d414bbaf5f724086cdcee893a9
Venkatesh Narayana Raja <110780431+venkysnow@users.noreply.github.com>
2022-08-11 01:07:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,17 +23,21 @@ fruits_to_show  = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlist.error("Please Select a fruit to get information. ")
+    else:
+      #streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      #streamlit.text(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 #  normalise the semi structured data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # frame for normalised response
-streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+except URLError as e:
+streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
