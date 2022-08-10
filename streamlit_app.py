4c298a2b38dd1aa190abd9531f29dda848548388
Venkatesh Narayana Raja <110780431+venkysnow@users.noreply.github.com>
2022-08-11 01:12:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
         streamlist.error("Please Select a fruit to get information. ")
     else:
       #streamlit.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       #streamlit.text(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
